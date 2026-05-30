3c22efc717f3e6f4a5baf1e21f469bb6c730187e
Nikhil-911 <nk4669469@gmail.com>
2026-05-30 12:21:25 +0530

    Added Day 3 programs

---
+++ Day_3/q12.cpp
@@ -0,0 +1,20 @@
+// C++ program to find LCM of two numbers
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b,max;
+    cout<<"enter first number: ";
+    cin>>a;
+    cout<<"enter second number: ";
+    cin>>b;
+    max=a>b?a:b;
+    while(true){
+        if(max%a==0 && max%b==0){
+            cout<<"LCM = "<< max;
+            break;
+        }
+        max++;
+    }
+    return 0;
+}
