cbdb6893c6df610770ef10d02dcfabcbc74f3eb8
Nikhil-911 <nk4669469@gmail.com>
2026-06-01 15:21:42 +0530

    Added Day 5 programs

---
+++ Day_5/q19.cpp
@@ -0,0 +1,17 @@
+// C++ program to find prime factors of anumber
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter number: ";
+    cin>>n;
+
+    cout<<"prime factors are: ";
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<" ";
+            n=n/i;
+        }
+    }
+    return 0;
+}
---
+++ Day_5/q20.cpp
@@ -0,0 +1,18 @@
+// C++ program to find largest prime factor of a number
+#include <iostream>
+using namespace std;
+int main() {
+    int n,largest;
+    cout<<"enter number: ";
+    cin>>n;
+
+    for(int i=2;i<=n;i++){
+        if(n%i==0){
+            largest=i;
+            n=n/i;
+
+        }
+    }
+    cout<<"largest prime factor = "<<largest<<endl;
+    return 0;
+}
