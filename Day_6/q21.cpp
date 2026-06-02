f29b6395224ce884d5cdf2eddc24b35513b934b4
Nikhil-911 <nk4669469@gmail.com>
2026-06-02 17:39:31 +0530

    Added Day 6 programs

---
+++ Day_6/q21.cpp
@@ -0,0 +1,16 @@
+// C++ program to convert decimal to binary number
+#include <iostream>
+using namespace std;
+int main(){
+    int decimal,binary=0,rem,pow=1;
+    cout<<"enter decimal number: ";
+    cin>>decimal;
+    while(decimal>0){
+        rem=decimal%2;
+        binary=binary+(rem*pow);
+        decimal=decimal/2;
+        pow=pow*10;
+    }
+    cout<<"binary: "<< binary <<endl;
+    return 0;
+}
---
+++ Day_6/q23.cpp
@@ -0,0 +1,17 @@
+// C++ program to count set bits in a number
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,count=0;
+    cout<<"enter a number: ";
+    cin>>n;
+
+    while(n>0){
+        if(n&1)
+            count++;
+        n=n>>1;
+    }
+    cout <<"set bits: "<< count <<endl;
+    return 0;
+}
