234bbd92bb8ee147b96c491da1124f1d88dbe7b9
Nikhil-911 <nk4669469@gmail.com>
2026-06-03 16:59:29 +0530

    added day 7 programs

---
+++ Day_7/q26.cpp
@@ -0,0 +1,19 @@
+//C++ program to find fibonacci of a number using recursion
+#include <iostream>
+using namespace std;
+int fibonacci(int n){
+    if(n<=1)
+    return n;
+
+    return fibonacci(n-1)+fibonacci(n-2);
+}
+
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+
+    cout<<"the fibonacci of " <<n << "is: " << fibonacci(n);
+    return 0;
+
+}
---
+++ Day_7/q27.cpp
@@ -0,0 +1,17 @@
+//C++ program to find sum of digits of a number using recursion
+#include <iostream>
+using namespace std;
+int sumOfDigit(int n){
+    if(n==0)
+    return 0;
+    
+
+    return (n%10)+sumOfDigit(n/10); 
+}
+int main(){
+    int n;
+    cout<<"enter a number: ";
+    cin>>n;
+    cout<<"sum of digits of "<<n <<"is: "<<sumOfDigit(n)<<endl;
+    return 0;
+}
