5e87acda65b6b31dc069ceaaf87441fd57b56265
Nikhil-911 <nk4669469@gmail.com>
2026-06-05 17:48:46 +0530

    Added Day 9 programs

---
+++ Day_9/33.cpp
@@ -0,0 +1,12 @@
+// C++ program to print the pattern
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=5;j>=i;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Day_9/34.cpp
@@ -0,0 +1,12 @@
+//C++ program to print the pattern
+#include <iostream>
+using namespace std;
+int main(){
+    for(int i=5;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<< j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
