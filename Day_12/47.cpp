0892c0fe07c53ff0023cbf6ae851a086e7920830
Nikhil-911 <nk4669469@gmail.com>
2026-06-08 17:32:21 +0530

    Added Day 12 programs

---
+++ Day_12/47.cpp
@@ -0,0 +1,19 @@
+//C++ program to print fibonacci series upto n terms
+#include <iostream>
+using namespace std;
+void fibonacci(int n){
+    int first=0,second=1,next;
+    cout<<"fibonacci series: ";
+    for(int i=1;i<=n;i++){
+        cout<<first <<" ";
+        next=first=second;
+        second=next;
+    }
+}
+int main(){
+    int n;
+    cout<<"enter number of terms: ";
+    cin>>n;
+    fibonacci(n);
+    return 0;
+}
