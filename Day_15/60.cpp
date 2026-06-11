55bb64af1703aa882927d022d75b5c91d21a9ee1
Nikhil-911 <nk4669469@gmail.com>
2026-06-11 17:09:57 +0530

    Added Day 15 programs

---
+++ Day_15/60.cpp
@@ -0,0 +1,27 @@
+//C++ program to rotate an array by one position towards right
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter array size: ";
+    cin>>n;
+    int arr[n];
+
+    cout<<"enter array element: ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int j=0;
+    for(int i=0; i<n;i++){
+        if (arr[i]!=0){
+            swap(arr[i],arr[j]);
+            j++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" "<<endl;
+    }
+
+    return 0;
+
+}
