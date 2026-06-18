ecdd0bc8823cfe652f75513f22cf0012a72ecdd0
Nikhil-911 <nk4669469@gmail.com>
2026-06-18 11:42:36 +0530

    Added Day 22 programs

---
+++ Day_22/85.cpp
@@ -0,0 +1,22 @@
+//C++ program to check whether a string is palindrome or not
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    string str,rev=" ";
+    cout<<"enter a string: ";
+    getline(cin,str);
+
+    bool palindrome = true;
+
+
+    for(int i=str.length()-1;i>=0;i--){
+        rev+=str[i];
+    }
+    if(str==rev){
+        cout<<"string is palindrome";
+    }else{
+        cout<<"string is not palindome";
+    }
+    return 0;
+}
---
+++ Day_22/88.cpp
@@ -0,0 +1,21 @@
+//C++ program to remove spaces from a string
+#include <iostream>
+using namespace std;
+int main(){
+    char str[100];
+    cout<<"enter a string";
+    cin.getline(str,100);
+    int j=0;
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]!=' '){
+            str[j]=str[i];
+        }
+
+    }
+    str[j]='\0';
+
+    cout<<"string without spaces: "<<str;
+
+    return 0; 
+
+}
