cfb04b60406dbed883bca23cdb5ee6fbf6ed84d4
Nikhil-911 <nk4669469@gmail.com>
2026-06-20 11:27:35 +0530

    Added Day 24 programs

---
+++ Day_24/94.cpp
@@ -0,0 +1,20 @@
+//C++ program to count the consecutive characters in a string
+#include <iostream>
+using namespace std;
+
+int main(){
+    char str[100];
+    cout<<"enter a string";
+    cin.getline(str,100);
+
+    for(int i=0;str[i]!='\0';i++){
+        int count=1;
+
+        while(str[i]==str[i+1]){
+            count++;
+            i++;
+        }
+        cout<<str[i]<<count;
+    }
+    return 0;
+}
